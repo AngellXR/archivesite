cab888e7fd25465a637a1965881bd45666a2fc14
jsealt <jsealt@users.noreply.github.com>
2020-08-21 18:42:00 -0400

    update blog

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -8,12 +8,16 @@ export default function FirstPost() {
       <Head>
         <title>First Post</title>
       </Head>
-      <h1>First Post</h1>
+      <h1>Part I: Path to MVP</h1>
       <h2>
         <Link href="/">
           <a>Back to home</a>
         </Link>
       </h2>
+      <p>
+        It's certainly been a long journey to get here, but I am proud to say,
+        we are living in a World that is finally ready for VR.
+      </p>
     </Layout>
   );
 }
