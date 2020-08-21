6b35553b36acfc2c8ccd9644fab856234d4a07e0
jsealt <jsealt@users.noreply.github.com>
2020-08-21 17:59:06 -0400

    setting up
    cloned pages from other blog, starting over to see what is broken

---
+++ nextjs-blog/pages/posts/first-post.js
@@ -0,0 +1,39 @@
+import Head from "next/head";
+import Link from "next/link";
+import Layout from "../../components/layout";
+import { Button } from "../../components/Button";
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterFollowButton,
+  TwitterHashtagButton,
+  TwitterMentionButton,
+  TwitterTweetEmbed,
+  TwitterMomentShare,
+  TwitterDMButton,
+  TwitterVideoEmbed,
+  TwitterOnAirButton,
+} from "react-twitter-embed";
+import InstagramEmbed from "react-instagram-embed";
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+      <TwitterFollowButton screenName={"angell_xr"} />
+      <TwitterTimelineEmbed
+        sourceType="profile"
+        screenName="angell_xr"
+        options={{ height: 800 }}
+      />
+    </Layout>
+  );
+}
---
+++ nextjs-blog/pages/posts/second-post.js
@@ -0,0 +1,3 @@
+export default function FirstPost() {
+  return <h1>Thanks for your interest in collaborating!</h1>;
+}
