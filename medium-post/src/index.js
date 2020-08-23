c48e05c1129e77350b7f6f5a2231705ff91ba2f1
jsealt <jsealt@users.noreply.github.com>
2020-08-23 16:56:00 -0400

    added bootstrap and css

--- medium-post/src/index.js
+++ medium-post/src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from ‘./components/App’;
 import * as serviceWorker from ‘./helpers/serviceWorker’;
+import ‘shards-ui/dist/css/shards.min.css’;
+import ‘bootstrap/dist/css/bootstrap.min.css’;
 
 ReactDOM.render(
   <React.StrictMode>
