d1b80fcc5152cea033764f2c2f37725d2e0d883d
jsealt <jsealt@users.noreply.github.com>
2020-08-24 16:56:29 -0400

    removed button
    failed to deploy because of components/button

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import Link from "next/link";
 import Layout from "../../components/layout";
-import { Button } from "../../components/Button";
 
 export default function FirstPost() {
   return (
