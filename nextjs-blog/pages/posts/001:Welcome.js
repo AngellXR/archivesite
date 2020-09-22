a617c7767a78b2b389dd2c34841bbb5e0c548363
jsealt <jsealt@users.noreply.github.com>
2020-09-22 17:43:15 -0400

    Update 001:Welcome.js
    add body text

--- nextjs-blog/pages/posts/001:Welcome.js
+++ nextjs-blog/pages/posts/001:Welcome.js
@@ -10,6 +10,10 @@ export default function APIPost() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>An Open Metaverse</h1>
+      <p>
+        It's certainly been a long journey to get here, but I am proud to say,
+        we are living in a World that is finally ready for XR.
+      </p>
       <h2>
         <Link href="/">
           <a>return home</a>
