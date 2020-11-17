491dad934f326da1c20914f4f0979b71610dd852
Jesse <63426722+jsealt@users.noreply.github.com>
2020-11-17 10:31:30 -0500

    test form

--- nextjs-blog/components/contact.js
+++ nextjs-blog/components/contact.js
@@ -105,3 +105,6 @@ class ContactForm extends Component {
   }
 }
 export default ContactForm;
+
+<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSe0aV18zeV-0Id2kC5gpgR5-tQnYge72fgdM9k_6o69j3Eeqg/viewform?embedded=true" width="640" height="911" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+
