ea0a7ad939c988a07da9861ee27bcaa4b58e9e66
Jesse <63426722+jsealt@users.noreply.github.com>
2020-11-17 10:38:55 -0500

    fixing mistake with form
    put the iframe in the wrong place. this should work.

--- nextjs-blog/components/contact.js
+++ nextjs-blog/components/contact.js
@@ -9,8 +9,6 @@ import {
   Label,
   Input,
 } from "reactstrap";
-// <!-- add google form test -->
-<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSe0aV18zeV-0Id2kC5gpgR5-tQnYge72fgdM9k_6o69j3Eeqg/viewform?embedded=true" width="640" height="911" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
 class ContactForm extends Component {
   state = {
     name: "",
--- nextjs-blog/pages/posts/API.js
+++ nextjs-blog/pages/posts/API.js
@@ -16,6 +16,9 @@ export default function APIPost() {
           <a>return home</a>
         </Link>
       </h2>
+      {/* add google form test */}
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSe0aV18zeV-0Id2kC5gpgR5-tQnYge72fgdM9k_6o69j3Eeqg/viewform?embedded=true" width="640" height="911" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+
       <ContactForm></ContactForm>
     </Layout>
   );
